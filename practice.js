400e1a5d03b976a3a46646402aafd61269fdb18a
jmitchell0528 <jlm0528@yahoo.com>
2017-02-16 17:57:24 -0600

    Completed THIS assignment :P

--- practice.js
+++ practice.js
@@ -6,14 +6,21 @@
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
 
       //Answer
+      // The four rules that govern the "this keyword" are:
+      // -Explicit - uses the call, apply, and bind methods, this rule is specifically attached.
+      // -Inplicit - is the object on the left of the period, and is referencing its surrounding objects.
+      // -Default - is used without an object on the method, and references global or (window).
+      // -New - Does not require a returned value, because it already knows to return the context
 
   // 3) What is the difference between call and apply?
 
       //Answer
+      // The difference between call and apply is, call can take an object and a certain amount  of arguments  and envoke the function immediately, while apply will take an object and an argument in an array and envoke the function immediately.
 
   // 4) What does .bind do?
 
       //Answer
+      // .bind will save a function definition to be invoked at a later time.
 
 
 //Next Problem
@@ -24,9 +31,15 @@
   //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
     //Code Here
-
+var user = {
+  username: "john",
+  email: "john@gmail.com",
+  getUsername: function() {
+    return this.username;
+  }
+}
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
-
+user.getUsername();
 
 //Next Problem
 
@@ -34,6 +47,16 @@
 // Write the function definitions which will make the following function invocations function properly.
 
   //Function Invocations Here
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.move = 0;
+  this.moveCar = function() {
+    this.move += 10;
+    return this.move;
+  }
+}
 
 var prius = new Car('Toyota', 'Prius', 2011);
 var mustang = new Car('Ford', 'Mustang', 2013);
@@ -69,7 +92,8 @@ var getMyUsername = function() {
  return this.username;
 };
 
-var userName = getMyUsername(); //Fix this
+var userName = getMyUsername.call(myUser);
+ //Fix this
 
 //Above you're given an object, and  a function. What will the getMyUsername function return?
 //Note(no tests)
